e339f197d96b0ae5a3fe788b8ee9937d157fa109
Aashish Kumar <kumar.ashish72809@gmail.com>
2024-09-15 00:32:35 +0530

    feat: Added helmet.js for security

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import path from "path";
 import apiRoutes from "./routers/index";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import wrongRouteHandler from "./middlewares/wrongRouteHandler";
+import helmet from "helmet";
 
 const app: Application = express();
 
 //middleware
+app.use(helmet());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../', 'public')));
 
