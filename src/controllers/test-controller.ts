63ef9dad5103a61232789e5d0ca5f053a353e4eb
Aashish Kumar <kumar.ashish72809@gmail.com>
2024-09-14 22:47:55 +0530

    feat: Added error handling logic

--- src/controllers/test-controller.ts
+++ src/controllers/test-controller.ts
@@ -7,8 +7,7 @@ import httpError from '../utils/httpError';
 export function test(req: Request, res: Response, next: NextFunction) {
     try {
         //get data from req.body or req.params
-        throw new Error("This is Error")
-        //do bussiness logic
+        //call the appropiate service for business logic and get the response
         httpResponse(req, res, StatusCodes.OK, responseMessage.SUCCESS, { id: 101} )
     } catch (error) {
         httpError(next, error, req, StatusCodes.INTERNAL_SERVER_ERROR)
--- src/utils/httpError.ts
+++ src/utils/httpError.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request } from "express";
 import errorObject from "./errorObject";
 
-export default function (next: NextFunction, error: Error | unknown, req: Request, errorStatusCode: number = 500): void {
+export default function (next: NextFunction, error: Error | unknown, req: Request, errorStatusCode: number = 500) {
     
     const errorObj = errorObject(error, req, errorStatusCode)
     
